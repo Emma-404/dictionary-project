a9ab4da9c9e6e3d93cc3425ddb88db554968aede
Emma-404 <emmamboronad@gmail.com>
2025-06-20 11:35:56 +0200

    Synonym component

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,17 +1,28 @@
 import React from "react";
+import Synonyms from "./Synonyms.js";
 
 export default function Meaning(props) {
   console.log(props.meaning);
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-      <div>
-        <p>
-          {props.meaning.definition}
-          <br />
-          <em>{props.meaning.example}</em>
-        </p>
-      </div>
+      {props.meaning.defintions.map(function (definition, index) {
+        return (
+          <div key={index}>
+            <p>
+              <strong>Definition:</strong>
+              {props.meaning.definition}
+              <br />
+              <strong>Example:</strong>
+              <em>{props.meaning.example}</em>
+              <br />
+              <Synonyms synonyms={definition.synonyms} />
+              <strong>Synonyms:</strong>
+              {props.definition.synonyms}
+            </p>
+          </div>
+        );
+      })}
     </div>
   );
 }
