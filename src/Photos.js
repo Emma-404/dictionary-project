d20fda963b3c768c6db518bc1dbe4b1e0c6869bd
Emma-404 <emmamboronad@gmail.com>
2025-06-23 09:28:51 +0200

    Debugged photo com
    yay!

--- src/Photos.js
+++ src/Photos.js
@@ -7,16 +7,23 @@ export default function Photos(props) {
       <section className="Photos">
         <div className="row">
           {props.photos.map(function (photo, index) {
-            return (
-              <div className="col-3" key={index}>
-                <a href={photo.src.original} target="_blank" rel="noreferrer" />
-                <img
-                  src={photo.src.landscape}
-                  alt={photo.alt}
-                  className="img-fluid"
-                />
-              </div>
-            );
+            if (index < 4) {
+              return (
+                <div className="col-3" key={index}>
+                  <a
+                    href={photo.src.original}
+                    target="_blank"
+                    rel="noreferrer"
+                  />
+                  <img
+                    src={photo.src.landscape}
+                    alt={photo.alt}
+                    className="img-fluid"
+                  />
+                </div>
+              );
+            }
+            return null;
           })}
         </div>
       </section>
